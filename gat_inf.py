5b827a365726a4f905308df2229c9e4c21537382
Wiqzard <s.stapf@aol.com>
2023-05-28 10:29:09 +0200

    added transformer and gatv2 crossattention support

--- gat_inf.py
+++ gat_inf.py
@@ -1,5 +1,7 @@
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, List
 import math
+import itertools
+from enum import Enum, auto
 
 import torch
 import torch.nn.functional as F
@@ -25,17 +27,81 @@ from torch_geometric.utils import (
     softmax,
 )
 from torch_geometric.utils.sparse import set_sparse_value
-from models.custom_layers import GeGLU
+from models.custom_layers import GeGLU, LearnedPositionalEmbeddings, GraphResNetBlock
 
+class AttentionMode(Enum):
+    """
+    The attention mechanism to use.
+    """
+    GAT = auto()
+    TRANS = auto()
+    
+class GraphNet(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        channels: int,
+        n_res_blocks: int,
+        attention_levels: List[int],
+        attention_mode: AttentionMode,
+        channel_multipliers: List[int],
+        n_heads: int,
+        tf_layers: int = 1,
+        d_cond: int = 1024,
+    ) -> None:
+        """
+        Args:
+            in_channels (int): The number of channels in the input feature map.
+            out_channels (int): The number of output channels.
+            channels (int): The base channel count for the model.
+            n_res_blocks (int): The number of residual blocks at each level.
+            attention_levels (List[int]): The levels at which attention should be performed.
+            channel_multiplier (List[int]): The factors for the number of channels at each level.
+            n_heads (int): The number of heads transformers.
+            tf_layers (int, optional): The number of transformer layers. Defaults to 1.
+            d_cond (int, optional): The dimension of the conditioning vector. Defaults to 768.
+        """
+        super().__init__()
+        self.channels = channels
+        levels = len(channel_multipliers)
+        d_time_emb = None
+        
+        self.pos_emb = LearnedPositionalEmbeddings(d_model=d_cond)
+        
+        self.input_blocks = nn.ModuleList([])
+        self.input_blocks.append(
+            GCNConv(in_channels, channels)
+        )
+        input_block_channels = [channels]
+        channels_list = [channels * m for m in channel_multipliers]
 
-class GATv2Conv(MessagePassing):
+        if attention_mode == AttentionMode.GAT:
+            attention_layer = Gatv2CrossAttention()
+        elif attention_mode == AttentionMode.TRANS:
+            attention_layer = TransformerCrossAttention() 
+        else:
+            raise ValueError(f"Unknown attention mode: {attention_mode}")
+
+        for i, _ in itertools.product(range(levels), range(n_res_blocks + 1)):
+            layers = [
+                GraphResNetBlock(channels, d_time_emb, out_channels=channels_list[i])
+            ]
+            channels = channels_list[i]
+
+            if i in attention_levels:
+                layers.append(InfusionTransformer(channels, n_heads, tf_layers, d_cond))
+            self.input_blocks.append(TimestepEmbedSequential(*layers))
+            input_block_channels.append(channels)
+
+class Gatv2CrossAttention(MessagePassing):
     _alpha: OptTensor
 
     def __init__(
         self,
         in_channels: Union[int, Tuple[int, int]],
         out_channels: int,
-        heads: int = 1,
+        n_heads: int = 1,
         d_cond: int = 1024,
         concat: bool = True,
         negative_slope: float = 0.2,
@@ -51,7 +117,7 @@ class GATv2Conv(MessagePassing):
 
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.heads = heads
+        self.n_heads = n_heads
         self.concat = concat
         self.d_cond = d_cond
         self.negative_slope = negative_slope
@@ -61,67 +127,42 @@ class GATv2Conv(MessagePassing):
         self.fill_value = fill_value
         self.share_weights = share_weights
 
-        if isinstance(in_channels, int):
-            self.lin_l = Linear(
+        self.lin_l = Linear(
+            in_channels,
+            n_heads * out_channels,
+            bias=bias,
+            weight_initializer="glorot",
+        )
+        if share_weights:
+            self.lin_r = self.lin_l
+        else:
+            self.lin_r = Linear(
                 in_channels,
-                heads * out_channels,
+                n_heads * out_channels,
                 bias=bias,
                 weight_initializer="glorot",
             )
-            if share_weights:
-                self.lin_r = self.lin_l
-            else:
-                self.lin_r = Linear(
-                    in_channels,
-                    heads * out_channels,
-                    bias=bias,
-                    weight_initializer="glorot",
-                )
-        else:
-            self.lin_l = Linear(
-                in_channels[0],
-                heads * out_channels,
-                bias=bias,
-                weight_initializer="glorot",
-            )
-            if share_weights:
-                self.lin_r = self.lin_l
-            else:
-                self.lin_r = Linear(
-                    in_channels[1],
-                    heads * out_channels,
-                    bias=bias,
-                    weight_initializer="glorot",
-                )
 
-        self.att = Parameter(torch.Tensor(1, heads, out_channels))
-        self.inf_k = nn.Linear(self.d_cond, heads * out_channels)
-        self.inf_v = nn.Linear(self.d_cond, heads * out_channels)
+        self.cross_attention = CrossAttention(
+            in_channels, d_cond, n_heads, out_channels, return_heads=True
+        )
+        # self.infusion_transformer = SpatialTransformer(out_channels, heads)
 
-        if edge_dim is not None:
-            self.lin_edge = Linear(
-                edge_dim, heads * out_channels, bias=False, weight_initializer="glorot"
-            )
-        else:
-            self.lin_edge = None
+        self.att = Parameter(torch.Tensor(1, n_heads, out_channels))
 
         if bias and concat:
-            self.bias = Parameter(torch.Tensor(heads * out_channels))
+            self.bias = Parameter(torch.Tensor(n_heads * out_channels))
         elif bias and not concat:
             self.bias = Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter("bias", None)
-
         self._alpha = None
-
         self.reset_parameters()
 
     def reset_parameters(self):
         super().reset_parameters()
         self.lin_l.reset_parameters()
         self.lin_r.reset_parameters()
-        if self.lin_edge is not None:
-            self.lin_edge.reset_parameters()
         glorot(self.att)
         zeros(self.bias)
 
@@ -133,26 +174,22 @@ class GATv2Conv(MessagePassing):
         edge_attr: OptTensor = None,
         return_attention_weights: bool = None,
     ):
-        H, C = self.heads, self.out_channels
-        BS = cond.shape[0]
+        H, C = self.n_heads, self.out_channels
 
         x_l: OptTensor = None
         x_r: OptTensor = None
 
         assert x.dim() == 2, "x should be [N_sum, d_in]"
         x_l = self.lin_l(x).view(-1, H, C)
-        if self.share_weights:
-            x_r = x_l
-        else:
-            x_r = self.lin_r(x).view(-1, H, C)
 
         if cond is not None:
             assert cond.dim() == 3, "cond should be [BS, N, d_cond]"
-            cond_k = self.inf_k(cond).view(BS, -1, H, C)
-            cond_v = self.inf_v(cond).view(BS, -1, H, C)
+            x_r = self.cross_attention(x, cond)
         else:
-            cond_k = None
-            cond_v = None
+            if self.share_weights:
+                x_r = x_l
+            else:
+                x_r = self.lin_r(x).view(-1, H, C)
 
         assert x_l is not None
         assert x_r is not None
@@ -232,70 +269,173 @@ class GATv2Conv(MessagePassing):
             x = x + edge_attr
 
         x = F.leaky_relu(x, self.negative_slope)
-        #        if cond_q_i is not None and cond_v_i is not None:
-        #            alpha = (x * cond_q_i).sum(dim=-1) / math.sqrt(self.out_channels)
-        #            alpha = softmax(alpha, index, ptr, size_i)
-        #            out = (alpha.unsqueeze(-1) * cond_v_i) * x_j
-        #        else:
         alpha = (x * self.att).sum(dim=-1)
         alpha = softmax(alpha, index, ptr, size_i)
 
         self._alpha = alpha
         alpha = F.dropout(alpha, p=self.dropout, training=self.training)
-        return alpha.unsqueeze(-1)
+        return x_j * alpha.unsqueeze(-1)
 
-    # return x_j * alpha.unsqueeze(-1)
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}({self.in_channels}, "
+            f"{self.out_channels}, heads={self.heads}, "
+            f"params={self.__num_parameters__}"
+        )
+
+
+class TransformerCrossAttention(MessagePassing):
+    def __init__(
+        self,
+        in_channels: Union[int, Tuple[int, int]],
+        out_channels: int,
+        n_heads: int = 1,
+        d_cond: int = 1024,
+        concat: bool = True,
+        beta: bool = False,
+        dropout: float = 0.0,
+        edge_dim: Optional[int] = None,
+        bias: bool = True,
+        root_weight: bool = True,
+        **kwargs,
+    ):
+        kwargs.setdefault("aggr", "add")
+        super().__init__(node_dim=0, **kwargs)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.n_heads = n_heads
+        self.d_cond = d_cond
+        self.beta = beta and root_weight
+        self.root_weight = root_weight
+        self.concat = concat
+        self.dropout = dropout
+        self.edge_dim = edge_dim
+        self._alpha = None
+
+        self.lin_query = Linear(in_channels, n_heads * out_channels)
+        self.lin_key = Linear(in_channels, n_heads * out_channels)
+        self.lin_value = Linear(in_channels, n_heads * out_channels)
+        #self.cross_attention = CrossAttention(out_channels, )
+        if edge_dim is not None:
+            self.lin_edge = Linear(edge_dim, n_heads * out_channels, bias=False)
+        else:
+            self.lin_edge = self.register_parameter("lin_edge", None)
+
+        if concat:
+            self.lin_skip = Linear(in_channels, n_heads * out_channels, bias=bias)
+            if self.beta:
+                self.lin_beta = Linear(3 * n_heads * out_channels, 1, bias=False)
+            else:
+                self.lin_beta = self.register_parameter("lin_beta", None)
+        else:
+            self.lin_skip = Linear(in_channels, out_channels, bias=bias)
+            if self.beta:
+                self.lin_beta = Linear(3 * out_channels, 1, bias=False)
+            else:
+                self.lin_beta = self.register_parameter("lin_beta", None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        super().reset_parameters()
+        self.lin_key.reset_parameters()
+        self.lin_query.reset_parameters()
+        self.lin_value.reset_parameters()
+        if self.edge_dim:
+            self.lin_edge.reset_parameters()
+        self.lin_skip.reset_parameters()
+        if self.beta:
+            self.lin_beta.reset_parameters()
+
+    def forward(
+        self,
+        x: Union[Tensor, PairTensor],
+        edge_index: Adj,
+        cond: Optional[Tensor] = None,
+        edge_attr: OptTensor = None,
+        return_attention_weights=None,
+    ):
+        H, C = self.n_heads, self.out_channels
+
+        query = self.lin_query(x).view(-1, H, C)
+        if cond is not None:
+            cond = self.cross_attention(x, cond)
+            cond = cond.view(*cond[:2], -1)
+            key = self.lin_key(cond).view(-1, H, C)
+            value = self.lin_value(cond).view(-1, H, C)
+        else:
+            key = self.lin_key(x).view(-1, H, C)
+            value = self.lin_value(x).view(-1, H, C)
+
+        # propagate_type: (query: Tensor, key:Tensor, value: Tensor, edge_attr: OptTensor) # noqa
+        out = self.propagate(
+            edge_index,
+            query=query,
+            key=key,
+            value=value,
+            edge_attr=edge_attr,
+            size=None,
+        )
+
+        alpha = self._alpha
+        self._alpha = None
+
+        if self.concat:
+            out = out.view(-1, self.heads * self.out_channels)
+        else:
+            out = out.mean(dim=1)
+
+        if self.root_weight:
+            x_r = self.lin_skip(x[1])
+            if self.lin_beta is not None:
+                beta = self.lin_beta(torch.cat([out, x_r, out - x_r], dim=-1))
+                beta = beta.sigmoid()
+                out = beta * x_r + (1 - beta) * out
+            else:
+                out = out + x_r
+
+        if isinstance(return_attention_weights, bool):
+            assert alpha is not None
+            if isinstance(edge_index, Tensor):
+                return out, (edge_index, alpha)
+            elif isinstance(edge_index, SparseTensor):
+                return out, edge_index.set_value(alpha, layout="coo")
+        else:
+            return out
+
+    def message(
+        self,
+        query_i: Tensor,
+        key_j: Tensor,
+        value_j: Tensor,
+        edge_attr: OptTensor,
+        index: Tensor,
+        ptr: OptTensor,
+        size_i: Optional[int],
+    ) -> Tensor:
+        if self.lin_edge is not None:
+            assert edge_attr is not None
+            edge_attr = self.lin_edge(edge_attr).view(-1, self.n_heads, self.out_channels)
+            key_j = key_j + edge_attr
+
+        alpha = (query_i * key_j).sum(dim=-1) / math.sqrt(self.out_channels)
+        alpha = softmax(alpha, index, ptr, size_i)
+        self._alpha = alpha
+        alpha = F.dropout(alpha, p=self.dropout, training=self.training)
+
+        out = value_j
+        if edge_attr is not None:
+            out = out + edge_attr
+
+        out = out * alpha.view(-1, self.n_heads, 1)
+        return out
 
     def __repr__(self) -> str:
         return (
             f"{self.__class__.__name__}({self.in_channels}, "
             f"{self.out_channels}, heads={self.heads})"
         )
-
-
-class SpatTransformer(nn.Module):
-    def __init__(self, in_channels, out_channels, n_heads, d_cond) -> None:
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.n_heads = n_heads
-        self.d_cond = d_cond
-
-        self.query_proj = nn.Linear(in_channels, n_heads * out_channels)
-        self.key_proj = nn.Linear(d_cond, n_heads * out_channels)
-        self.value_proj = nn.Linear(d_cond, n_heads * out_channels)
-
-        # self.query_proj = nn.Linear(d_cond, n_heads * out_channels)
-        # self.key_proj = nn.Linear(in_channels, n_heads * out_channels)
-        # self.value_proj = nn.Linear(in_channels, n_heads * out_channels)
-
-    def forward(self, x, cond=None):
-        # cond shape: (batch_size, seq_len, cond_dim)
-        # x shape: (sum(N_i), in_channels)
-
-        assert cond is not None, "cond must be provided"
-        if cond is None:
-            cond = torch.zeros(x.shape[0], 1, 1)
-        else:
-            # important to work with padded graphs or fixed size graphs
-            bs = cond.shape[0]
-            assert x.shape[0] % bs == 0, "batch size must be a divisor of x.shape[0]"
-            # transform features
-            # makes use of positional invariance of transformer
-            # pad graphs with zero vectors to allow for batch processing
-            # split graph into batch of graphs (batch_size, num_nodes, in_channels)
-            x = torch.stack(torch.chunk(x, bs, dim=0))
-            query = self.query_proj(x).view(*x.shape[:2], self.n_heads, -1)
-            key = self.key_proj(cond).view(*cond.shape[:2], self.n_heads, -1)
-            value = self.value_proj(cond).view(*cond.shape[:2], self.n_heads, -1)
-            attn = torch.einsum("bnhd,bmhd->bhnm", query, key) / math.sqrt(
-                self.out_channels
-            )
-            attn_weights = F.softmax(attn, dim=-1)
-            out = torch.einsum("bhnm,bmhd->bnhd", attn_weights, value)
-            out = out.reshape(*out.shape[:2], -1)
-            out = out.reshape(-1, out.shape[-1])  # (sum(N_i), out_channels)
-        return 0
 
 
 class SpatialTransformer(nn.Module):
@@ -334,7 +474,9 @@ class BasicTransformerBlock(nn.Module):
 
         # self.attn1 = CrossAttention(d_model, d_model, n_heads, d_head)
         # self.norm1 = nn.LayerNorm(d_model)
-        self.attn2 = CrossAttention(d_model, d_cond, n_heads, d_head)
+        self.attn2 = CrossAttention(
+            d_model, d_cond, n_heads, d_head, return_heads=False
+        )
         self.norm2 = nn.LayerNorm(d_model)
 
         self.net = nn.Sequential(
@@ -353,11 +495,19 @@ class BasicTransformerBlock(nn.Module):
 
 
 class CrossAttention(nn.Module):
-    def __init__(self, d_model: int, d_cond: int, n_heads: int, d_head: int) -> None:
+    def __init__(
+        self,
+        d_model: int,
+        d_cond: int,
+        n_heads: int,
+        d_head: int,
+        return_heads: Optional[bool] = True,
+    ) -> None:
         super().__init__()
         self.d_model = d_model
         self.n_heads = n_heads
         self.d_head = d_head
+        self.return_heads = return_heads
 
         self.scale = d_head**-0.5
 
@@ -379,8 +529,13 @@ class CrossAttention(nn.Module):
         attn = torch.einsum("bnhd,bmhd->bhnm", q, k) * self.scale
         attn_weights = F.softmax(attn, dim=-1)
         out = torch.einsum("bhnm,bmhd->bnhd", attn_weights, v)
-        out = out.reshape(*out.shape[:2], -1)
-        return self.out_proj(out).reshape(-1, self.d_model)
+
+        if self.return_heads:
+            return out.reshape(-1, self.n_heads, self.d_head)
+        else:
+            out = out.reshape(*out.shape[:2], -1)
+            out = self.out_proj(out)
+            return out.reshsape(-1, self.d_model)
 
 
 import torch
@@ -388,18 +543,26 @@ from data_tools.graph_tools.graph import Graph
 
 
 def main() -> int:
-    gat_v2 = GATv2Conv(16, 32, heads=4, concat=False)  # concat True, returns 128
     graph = Graph.create_random_graph(100, 16)
     graph.set_edge_index()
     x = torch.from_numpy(graph.feature_matrix)
     edge_index = torch.from_numpy(graph.edge_index).T
-    cond = torch.randn(10, 47, 1024)
+    cond = torch.randn(10, 49, 1024)
+    gat_v2 = Gatv2CrossAttention(
+        16, 32, n_heads=8, concat=False
+    )  # concat True, returns 128
+    trans = TransformerCrossAttention(16, 32, n_heads=8, concat=False)
+
     # out = gat_v2(x, edge_index, cond)
-    cross = CrossAttention(16, 1024, 4, 32)
-    spat_trans = SpatialTransformer(16, 4, 4, 1024)
+    out2 = trans(x, edge_index, cond)
+    # cross = CrossAttention(16, 1024, 4, 32)
+    # cross_out = cross(x, cond)
+    # spat_trans = SpatialTransformer(16, 4, 4, 1024)
     # print number of parameters
-    print( sum(p.numel() for p in spat_trans.parameters()))
-    out = spat_trans(x, edge_index, cond)
+    print("gatv2: ", sum(p.numel() for p in gat_v2.parameters()))
+    print("transformer: ", sum(p.numel() for p in trans.parameters()))
+    # print( sum(p.numel() for p in spat_trans.parameters()))
+    # out = spat_trans(x, edge_index, cond)
     #   out = cross(x, cond)
     # out2 = SpatTransformer(16, 32, 4, 1024)(x, cond)
 
--- models/custom_layers.py
+++ models/custom_layers.py
@@ -1,6 +1,14 @@
+from collections import OrderedDict
+from typing import Optional
+
+
 import torch
-import torch.nn as nn   
+import torch.nn as nn
+from torch.nn import Sequential
+from torch import Tensor
 import torch.nn.functional as F
+
+from torch_geometric.nn import GCNConv
 
 
 class GeGLU(nn.Module):
@@ -20,3 +28,85 @@ class GeGLU(nn.Module):
         x, gate = self.proj(x).chunk(2, dim=-1)
         # $\text{GeGLU}(x) = (xW + b) * \text{GELU}(xV + c)$
         return x * F.gelu(gate)
+
+
+class LearnedPositionalEmbeddings(nn.Module):
+    def __init__(self, d_model: int, max_len: int = 5000) -> None:
+        super().__init__()
+        self.positional_encodings = nn.Parameter(
+            torch.zeros(max_len, 1, d_model), requires_grad=True
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x + self.positional_encodings[: x.shape[0]]
+
+
+
+class GraphResNetBlock(nn.Module):
+    """A graph residual network layer (GraphResNet)"""
+
+    def __init__(
+        self,
+        channels: int,
+        *,
+        d_t_emb: Optional[int] = None,
+        out_channels: Optional[int] = None
+    ):
+        """
+        Args:
+            in_features (int): The number of input features.
+            out_features (int): The number of output features.
+            bias (bool, optional): If set to True, the layer will learn an additive bias. Defaults to True.
+        """
+        super().__init__()
+        if not out_channels:
+            out_channels = channels
+        self.in_conv = Sequential(
+            "x, edge_index",
+            [
+                (nn.BatchNorm1d(channels), "x -> x1"),
+                (nn.Dropout(p=0.0), "x1 -> x1"),
+                (GCNConv(channels, out_channels), "x1, edge_index -> x2"),
+                (nn.SiLU(), "x2 -> x2"),
+            ],
+        )
+        self.out_conv = Sequential(
+            "x, edge_index",
+            [
+                (nn.BatchNorm1d(out_channels), "x -> x1"),
+                (nn.Dropout(p=0.0), "x1 -> x1"),
+                (GCNConv(out_channels, out_channels), "x1, edge_index -> x2"),
+                (nn.SiLU(), "x2 -> x2"),
+            ],
+        )
+        if d_t_emb is not None:
+            self.emb_layers = nn.Sequential(
+                OrderedDict(
+                    [
+                        ("emb_fc1", nn.Linear(d_t_emb, out_channels)),
+                        ("act", nn.SiLU()),
+                    ]
+                )
+            )
+
+        if out_channels == channels:
+            self.skip_connection = None  # nn.Identity()
+        else:
+            self.skip_connection = GCNConv(channels, out_channels)
+
+    def forward(
+        self,
+        x: Tensor,
+        edge_index: Tensor,
+        t_emb: Optional[Tensor] = None,
+    ):
+        h = self.in_conv(x, edge_index)
+        if t_emb is not None:
+            t_emb = self.emb_layers(t_emb).type(h.dtype)
+            h += t_emb  # [: ,: ,]
+        h = self.out_conv(h, edge_index)
+        return (
+            self.skip_connection(x, edge_index) + h
+            if self.skip_connection is not None
+            else x + h
+        )
